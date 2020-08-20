2880e545594187e116fed46e122658e3056ce43f
yujeong0 <kos9811@naver.com>
2020-08-20 23:37:15 +0900

    commit

--- src/Baekjoon/Baekjoon_1003_피보나치함수.java
+++ src/Baekjoon/Baekjoon_1003_피보나치함수.java
@@ -3,24 +3,26 @@ package Baekjoon;
 import java.util.Scanner;
 
 public class Baekjoon_1003_피보나치함수 {
-	private static int[] arr = { 0, 0 };
-	private static int[] arrN = new int[41];
+	private static int[][] arr = new int[41][2];
+	private static int[] f = new int[41];
 	private static int N;
 	
 	private static int fibo(int n) {
 		if(n == 0) {
-			arr[0]++;
 			return 0;
 		}
 		if(n == 1) {
-			arr[1]++;
-			return arrN[N] = 1;
+			return f[n] = 1;
 		}
+		
+		if(f[n] > 0) return f[n];
+		
+		f[n] = fibo(n-1) + fibo(n-2);
 
-		if(arrN[N] > 0) return arrN[N];
+		arr[n][0] = arr[n-1][0] + arr[n-2][0];
+		arr[n][1] = arr[n-1][1] + arr[n-2][1];
 		
-		
-		return arrN[N] = fibo(n-1) + fibo(n-2);
+		return f[n];
 	}
 	
 	public static void main(String[] args) {
@@ -30,12 +32,12 @@ public class Baekjoon_1003_피보나치함수 {
 		
 		int T = sc.nextInt();
 		for (int i = 0; i < T; i++) {
-			arr[0] = 0;
-			arr[1] = 0;
+			arr[0][0] = 1;
+			arr[1][1] = 1;
 			N = sc.nextInt();
 			fibo(N);
 
-			sb.append(arr[0] + " " + arr[1] + "\n");
+			sb.append(arr[N][0] + " " + arr[N][1] + "\n");
 		}
 		
 		System.out.println(sb.toString());
---
+++ src/Baekjoon/Baekjoon_9205_맥주마시면서걸어가기.java
@@ -0,0 +1,9 @@
+package Baekjoon;
+
+public class Baekjoon_9205_맥주마시면서걸어가기 {
+	public static void main(String[] args) {
+		
+		
+		
+	}
+}
