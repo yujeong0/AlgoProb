97ca7e0d69e64a8289a47216bd7284b82f528079
yujeong0 <kos9811@naver.com>
2020-10-11 03:00:55 +0900

    commit

--- src/SWexpert/모의역량/SW_5656_벽돌깨기2.java
+++ src/SWexpert/모의역량/SW_5656_벽돌깨기2.java
@@ -12,12 +12,12 @@ import java.util.StringTokenizer;
 public class SW_5656_벽돌깨기2 {
 
 	static int N, W, H;
-	static int[][] grid;
+	static int[][] grid = new int[16][13];
 	static int[][] dir = { {-1, 0}, {0, 1}, {1, 0}, {0, -1} };
 	static int count;
-	static List<int[]> crushedList = new ArrayList<>();
 	static int MIN;
-	static boolean[][] visited;
+	static boolean[][] visited = new boolean[16][13];
+	static Queue<int[]> q = new LinkedList<>();
 	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -31,8 +31,6 @@ public class SW_5656_벽돌깨기2 {
 			N = Integer.parseInt(st.nextToken());
 			W = Integer.parseInt(st.nextToken());
 			H = Integer.parseInt(st.nextToken());
-			grid = new int[H][W];
-			visited = new boolean[H][W];
 			
 			for (int i = 0; i < H; i++) {
 				st = new StringTokenizer(br.readLine(), " ");
@@ -41,16 +39,18 @@ public class SW_5656_벽돌깨기2 {
 				}
 			} // end of input
 			
-			visited = new boolean[H][W];
 			MIN = Integer.MAX_VALUE;
 
-			int[][] arr = new int[H][];
-			
 			for (int j = 0; j < W; j++) {
+				if(j == 9)
+					System.out.println();
 				for (int i = 0; i < H; i++) {
 					if(grid[i][j] != 0) {
+						int[][] arr = new int[H][W];
 						for (int k = 0; k < H; k++) {
-							arr[k] = Arrays.copyOf(grid[k], W);
+							for (int k2 = 0; k2 < W; k2++) {
+								arr[k][k2] = grid[k][k2];
+							}
 						}
 						solve(arr, i, j, N);
 						break;
@@ -68,8 +68,10 @@ public class SW_5656_벽돌깨기2 {
 	private static void solve(int[][] arr, int startx, int starty, int marble) {
 		if(marble == 0) {
 			count = countBlock(arr);
-//			System.out.println(count);
-//			printArr(arr);
+//			if(count == 26) {
+//				System.out.println(count);
+//				printArr(arr);
+//			}
 			if(MIN > count) MIN = count;
 			
 			return;
@@ -77,66 +79,51 @@ public class SW_5656_벽돌깨기2 {
 		
 		for (int i = 0; i < H; i++)
 			Arrays.fill(visited[i], false);	
+					
+		q.clear();
+		int[][] copy = new int[H][W];
+		for (int k = 0; k < H; k++) {
+			for (int k2 = 0; k2 < W; k2++) {
+				copy[k][k2] = arr[k][k2];
+			}
+		}
+		bfs(startx, starty, copy);	// 구슬 하나 떨어뜨리기~
 		
-		for (int c = 0; c < W; c++) {
-			for (int r = 0; r < H; r++) {
-				
-				if(c == starty && r == startx) {
-//					visited[r][c] = true;
-//					crushedList.add(new int[] {r, c});
-					
-					int[][] copy = new int[H][];
-					for (int k = 0; k < H; k++) {
-						copy[k] = Arrays.copyOf(arr[k], W);
+		// 배열 떙기기~
+		for (int col = 0; col < W; col++) {
+			for (int row = H-1; row > 0; row--) {
+				if(copy[row][col] == 0) {
+					for(int x = row; x > 0; x--) {
+						copy[x][col] = copy[x-1][col];
 					}
-//					dfs(r, c, copy);
-//					for(int[] pos : crushedList) {
-//						arr[pos[0]][pos[1]] = 0;	// 블록제거하기
-//					}
-//					crushedList.clear();
-					
-					bfs(r, c, arr);
-					
-					
-					// 배열 떙기기~
-					for (int col = 0; col < W; col++) {
-						for (int row = H-1; row > 0; row--) {
-							if(arr[row][col] == 0) {
-								for(int x = row; x > 0; x--) {
-									arr[x][col] = arr[x-1][col];
-								}
-								arr[0][col] = 0;
-							}
+					copy[0][col] = 0;
+				}
+			}
+		}
+		
+		// 그 다음 구슬 던지러 go
+		for (int j = 0; j < W; j++) {
+			for (int i = 0; i < H; i++) {
+				if(copy[i][j] != 0) {
+					int[][] copyArr = new int[H][W];
+					for (int k = 0; k < H; k++) {
+						for (int k2 = 0; k2 < W; k2++) {
+							copyArr[k][k2] = copy[k][k2];
 						}
 					}
-					
-					// 그 다음 구슬 던지러 go
-					for (int j = 0; j < W; j++) {
-						for (int i = 0; i < H; i++) {
-							if(arr[i][j] != 0) {
-								int[][] copyArr = new int[H][];
-								for (int k = 0; k < H; k++) {
-									copyArr[k] = Arrays.copyOf(arr[k], W);
-								}
-								solve(copyArr, i, j, marble-1);
-								break;
-							}
-						}	
-					}
-			
+					solve(copyArr, i, j, marble-1);
 					break;
-				} // if
-				
-			} // r
-		} // c
+				}
+			}	
+		}
+
 		
 	} // solve
 	
 	private static void bfs(int startx, int starty, int[][] arr) {
-		Queue<int[]> q = new LinkedList<>();
+
 		q.offer(new int[] {startx, starty});
 		visited[startx][starty] = true;
-		crushedList.add(new int[] {startx, starty});
 		
 		int[] pos;
 		int x, y, distance;
@@ -153,37 +140,13 @@ public class SW_5656_벽돌깨기2 {
 					x += dir[d][0];
 					y += dir[d][1];
 					
-					if(x >= 0 && x < H && y < W && y >= 0 && !visited[x][y] && arr[x][y] != 0) {
-						crushedList.add(new int[] {x, y});
-						visited[x][y] = true;
-						if(arr[x][y] != 1)	// 1이면 자기만 제거되니까 dfs 안들어감
+					if(x >= 0 && x < H && y < W && y >= 0) {
+						if(!visited[x][y] && arr[x][y] != 0) {
+							visited[x][y] = true;
 							q.offer(new int[] {x, y});
+						}
 					}
-				}
-				
-			}
-			
-		}
-		
-	}
-	
-	private static void dfs(int startx, int starty, int[][] arr) {
-		int x, y;
-		int distance = arr[startx][starty];
-		
-		for (int d = 0; d < 4; d++) {
-			x = startx;
-			y = starty;
-			
-			for (int dis = 0; dis < distance-1; dis++) {	// 제거되는 범위까지만
-				x += dir[d][0];
-				y += dir[d][1];
-				
-				if(x >= 0 && x < H && y < W && y >= 0 && !visited[x][y] && arr[x][y] != 0) {
-					crushedList.add(new int[] {x, y});
-					visited[x][y] = true;
-					if(arr[x][y] != 1)	// 1이면 자기만 제거되니까 dfs 안들어감
-						dfs(x, y, arr);
+					else break;
 				}
 				
 			}
