47240cb916211f38cb3acc8af3a058db7fb475a0
yujeong0 <kos9811@naver.com>
2020-10-11 17:16:44 +0900

    commit

--- src/SWexpert/모의역량/SW_5656_벽돌깨기.java
+++ src/SWexpert/모의역량/SW_5656_벽돌깨기.java
@@ -13,7 +13,7 @@ public class SW_5656_벽돌깨기 {
 	static int[][] dir = { {-1, 0}, {0, 1}, {1, 0}, {0, -1} };
 	static int count;
 	static int MIN;
-//	static boolean[][] visited = new boolean[20][20];
+	static boolean[][] visited = new boolean[20][20];
 	static Queue<int[]> q = new LinkedList<>();
 	
 	public static void main(String[] args) throws Exception {
@@ -28,17 +28,17 @@ public class SW_5656_벽돌깨기 {
 			W = Integer.parseInt(st.nextToken());
 			H = Integer.parseInt(st.nextToken());
 			
-			for (int i = 0; i < H; i++) {
+			for (int i = 1; i <= H; i++) {
 				st = new StringTokenizer(br.readLine(), " ");
-				for (int j = 0; j < W; j++) {
+				for (int j = 1; j <= W; j++) {
 					grid[i][j] = Integer.parseInt(st.nextToken());
 				}
 			} // end of input
 			
 			// 처음부터 블럭이 0개일 경우엔 바로 MIN을 0으로
 			boolean isEnd = true;
-			for (int k = H-1; k >= 0; k--) {
-				for (int k2 = 0; k2 < W; k2++) {
+			for (int k = H; k >= 1; k--) {
+				for (int k2 = 1; k2 <= W; k2++) {
 					if(grid[k][k2] != 0) {
 						isEnd = false;
 						break;
@@ -51,12 +51,12 @@ public class SW_5656_벽돌깨기 {
 			else {
 				MIN = Integer.MAX_VALUE;
 	
-			out:for (int j = 0; j < W; j++) {
-					for (int i = 0; i < H; i++) {
+			out:for (int j = 1; j <= W; j++) {
+					for (int i = 1; i <= H; i++) {
 						if(grid[i][j] != 0) {
-							int[][] arr = new int[H][W];
-							for (int k = 0; k < H; k++) {
-								for (int k2 = 0; k2 < W; k2++) {
+							int[][] arr = new int[H+1][W+1];
+							for (int k = 1; k <= H; k++) {
+								for (int k2 = 1; k2 <= W; k2++) {
 									arr[k][k2] = grid[k][k2];
 								}
 							}
@@ -83,9 +83,9 @@ public class SW_5656_벽돌깨기 {
 			return;
 		}
 		
-		int[][] copy = new int[H][W];
-		for (int k = 0; k < H; k++) {
-			for (int k2 = 0; k2 < W; k2++) {
+		int[][] copy = new int[H+1][W+1];
+		for (int k = 1; k <= H; k++) {
+			for (int k2 = 1; k2 <= W; k2++) {
 				copy[k][k2] = arr[k][k2];
 			}
 		}
@@ -93,8 +93,8 @@ public class SW_5656_벽돌깨기 {
 		
 		// 이미 배열이 다 0일 경우 바로 종료해버리기
 		boolean isEnd = true;
-		for (int k = H-1; k >= 0; k--) {
-			for (int k2 = 0; k2 < W; k2++) {
+		for (int k = H; k >= 1; k--) {
+			for (int k2 = 1; k2 <= W; k2++) {
 				if(copy[k][k2] != 0) {
 					isEnd = false;
 					break;
@@ -107,12 +107,12 @@ public class SW_5656_벽돌깨기 {
 		}
 		
 		// 배열 떙기기~
-		for (int col = 0; col < W; col++) {
-			for (int row = H-1; row > 0; row--) {
+		for (int col = 1; col <= W; col++) {
+			for (int row = H; row > 1; row--) {
 				if(copy[row][col] == 0) {
 					// 기준 행 위로 다 0 이면 종료하고 아니면 그대로 진행
 					boolean isZero = true;
-					for (int x = row-1; x > 0; x--) {
+					for (int x = row-1; x >= 1; x--) {
 						if(copy[x][col] != 0) {
 							isZero = false;
 							break;
@@ -121,10 +121,10 @@ public class SW_5656_벽돌깨기 {
 					if(isZero) break;
 					
 					// 배열 한 칸씩 내림
-					for(int x = row; x > 0; x--) {
+					for(int x = row; x > 1; x--) {
 						copy[x][col] = copy[x-1][col];
 					}
-					copy[0][col] = 0;
+					copy[1][col] = 0;
 					
 					row++;	// 다시 자신부터 0인지 확인해야하기때문에 +1 해줌
 					
@@ -134,12 +134,12 @@ public class SW_5656_벽돌깨기 {
 		}	// col
 		
 		// 그 다음 구슬 던지러 go
-		for (int j = 0; j < W; j++) {
-			for (int i = 0; i < H; i++) {
+		for (int j = 1; j <= W; j++) {
+			for (int i = 1; i <= H; i++) {
 				if(copy[i][j] != 0) {
-					int[][] copyArr = new int[H][W];
-					for (int k = 0; k < H; k++) {
-						for (int k2 = 0; k2 < W; k2++) {
+					int[][] copyArr = new int[H+1][W+1];
+					for (int k = 1; k <= H; k++) {
+						for (int k2 = 1; k2 <= W; k2++) {
 							copyArr[k][k2] = copy[k][k2];
 						}
 					}
@@ -154,16 +154,16 @@ public class SW_5656_벽돌깨기 {
 	} // solve
 	
 	private static void bfs(int startx, int starty, int[][] arr) {	// 구슬떨어뜨렸을 떄 제거되는 것들 0으로 만들기
-//		for (int i = 0; i < H; i++) {
-//			for (int j = 0; j < W; j++) {
-//				visited[i][j] = false;	
-//			}
-//		}
+		for (int i = 1; i <= H; i++) {
+			for (int j = 1; j <= W; j++) {
+				visited[i][j] = false;	
+			}
+		}
 					
 		q.clear();
 		
 		q.offer(new int[] {startx, starty});
-//		visited[startx][starty] = true;
+		visited[startx][starty] = true;
 		
 		int[] pos;
 		int x, y, distance;
@@ -182,28 +182,24 @@ public class SW_5656_벽돌깨기 {
 					x += dir[d][0];
 					y += dir[d][1];
 					
-					if(x >= 0 && x < H && y < W && y >= 0) {
-						if(arr[x][y] != 0) {
-							q.offer(new int[] {x, y});
-						}
-//						if(!visited[x][y] && arr[x][y] != 0) {
-//							visited[x][y] = true;
-//							q.offer(new int[] {x, y});
-//						}
+					if(x < 1 || x >= H+1 || y >= W+1 || y < 1) continue;
+					
+					if(!visited[x][y] && arr[x][y] != 0) {
+						visited[x][y] = true;
+						q.offer(new int[] {x, y});
 					}
-					else break;
 				}
 				
 			}
 			
 		}
 		
-	}
+	} // bfs
 	
 	private static int countBlock(int[][] arr) {
 		int sum = 0;
-		for (int i = 0; i < H; i++) {
-			for (int j = 0; j < W; j++) {
+		for (int i = 1; i <= H; i++) {
+			for (int j = 1; j <= W; j++) {
 				if(arr[i][j] != 0) sum++;
 			}
 		}
@@ -211,14 +207,4 @@ public class SW_5656_벽돌깨기 {
 		return sum;
 	}
 	
-//	private static void printArr(int[][] arr) {
-//		for (int i = 0; i < H; i++) {
-//			for (int j = 0; j < W; j++) {
-//				System.out.print(arr[i][j] + " ");
-//			}
-//			System.out.println();
-//		}
-//		System.out.println();
-//	}
-
 } // end of class
