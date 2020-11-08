c4ffd66c0030667a51fda64464552c502bc5fed7
yujeong0 <kos9811@naver.com>
2020-11-08 10:50:21 +0900

    commit

--- src/Baekjoon/Baekjoon_15685_드래곤커브.java
+++ src/Baekjoon/Baekjoon_15685_드래곤커브.java
@@ -1,10 +1,75 @@
 package Baekjoon;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
 public class Baekjoon_15685_드래곤커브 {
 
-	public static void main(String[] args) {
+	static int N;
+	static int[][] dragon;
+	static int[][] grid = new int[101][101];
+	static int[][] dir = { {1, 0}, {0, -1},{-1, 0} ,{0, 1} };
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		dragon = new int[N][4];
 		
+		StringTokenizer st;
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			dragon[i][0] = Integer.parseInt(st.nextToken());
+			dragon[i][1] = Integer.parseInt(st.nextToken());
+			dragon[i][2] = Integer.parseInt(st.nextToken());
+			dragon[i][3] = Integer.parseInt(st.nextToken());
+		}
+		
+		solve();
+		
+		System.out.println(getRect());
 	} //main
 	
+	private static void solve() {
+		for (int i = 0; i < N; i++) { // 커브 수만큼
+			
+			List<Integer> list = new ArrayList<>();
+			list.add(dragon[i][2]);
+			for (int gen = 1; gen <= dragon[i][3]; gen++) {
+				for(int d = list.size()-1; d >= 0; d--) {
+					list.add((list.get(d)+1) % 4);
+				}
+			} // gen
+			
+			int x = dragon[i][0];
+			int y = dragon[i][1];
+			grid[y][x] = 1;
+			
+			for (int d : list) {
+				x += dir[d][0];
+				y += dir[d][1];
+				
+				grid[y][x] = 1;
+			}
+			
+		}
+		
+	} // solve
+	
+	private static int getRect() {
+		int cnt = 0;
+		for (int i = 0; i < 100; i++) {
+			for (int j = 0; j < 100; j++) {
+				if(grid[i][j] == 1 && grid[i+1][j] == 1 && grid[i][j+1] == 1 && grid[i+1][j+1] == 1) {
+					cnt++;
+				}
+			
+			}
+		}
+		
+		return cnt;
+	}
 	
 } // class
--- src/SWexpert/Professional/SW_Professional_5643_키순서.java
+++ src/SWexpert/Professional/SW_Professional_5643_키순서.java
@@ -2,8 +2,6 @@ package SWexpert.Professional;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.HashSet;
-import java.util.Set;
 import java.util.StringTokenizer;
 
 /*
@@ -11,9 +9,9 @@ import java.util.StringTokenizer;
  */
 public class SW_Professional_5643_키순서 {
 	
-	static int[][] adj;
+	static int[][] talladj, shortadj;
 	static int N, M;
-	static int cnt;
+	static int tallCnt, shortCnt;
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
@@ -23,50 +21,55 @@ public class SW_Professional_5643_키순서 {
 		for (int testcase = 1; testcase <= T; testcase++) {
 			N = Integer.parseInt(br.readLine());
 			M = Integer.parseInt(br.readLine());
-			
-			adj = new int[N+1][N+1];
+			talladj = new int[N+1][N+1];
+			shortadj = new int[N+1][N+1];
 			
 			int a, b;
 			for (int i = 0; i < M; i++) {
 				st = new StringTokenizer(br.readLine(), " ");
 				a = Integer.parseInt(st.nextToken());
 				b = Integer.parseInt(st.nextToken());
-				adj[a][b] = 1;
+				talladj[a][b] = 1;
+				shortadj[b][a] = 1;
 			}
 
-			cnt = 0;
+			int answer = 0;
 			for (int i = 1; i <= N; i++) {
-				for (int j = 1; j <= N; j++) {
-					boolean[] visited = new boolean[N+1];
-					dfs(1, 1, visited);
-				}
+				tallCnt = shortCnt = 0;
+				tallDfs(i, new boolean[N+1]);
+				shortDfs(i, new boolean[N+1]);
+				
+				if(tallCnt + shortCnt == N-1) answer++;
 			}
 			
-			for (int i = 1; i <= N; i++) {
-				for (int j = 1; j <= N; j++) {
-				}
-			}
-			
-			sb.append("#").append(testcase).append(" ").append(cnt).append("\n");
+			sb.append("#").append(testcase).append(" ").append(answer).append("\n");
 		}
 		
 		System.out.println(sb.toString());
 	} // main
 	
-	private static void dfs(int start, int cur, boolean[] visited) {
-		visited[cur] = true;
-		for (int i = 1; i <= N; i++) {
-			if(start == cur || start == i || cur == i) continue;
+	private static void tallDfs(int start, boolean[] visited) {
+		visited[start] = true;
+		for (int j = 1; j <= N; j++) {
+			if(start == j) continue;
 			
-			if(adj[start][cur] == 1 && adj[cur][i] == 1 && !visited[cur]) {
-				cnt++;
-				adj[cur][i] = 1;
-				adj[i][start] = 1;
-				dfs(start, i, visited);
+			if(talladj[start][j] == 1 && !visited[j]) {
+				tallCnt++;
+				tallDfs(j, visited);
 			}
-			
 		}
-		
+	}
+	
+	private static void shortDfs(int start, boolean[] visited) {
+		visited[start] = true;
+		for (int j = 1; j <= N; j++) {
+			if(start == j) continue;
+			
+			if(shortadj[start][j] == 1 && !visited[j]) {
+				shortCnt++;
+				shortDfs(j, visited);
+			}
+		}
 	}
 	
 } // class
