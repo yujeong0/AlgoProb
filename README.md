728eee26f1ebb84b326c09d4612cd2a3b3c28fa3
yujeong0 <kos9811@naver.com>
2020-12-19 13:12:16 +0900

    commit

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # AlgoProb
 백준, SW expert academy, 정올, programmers
-문제 풀이 코드.
+문제 풀이 코드
--- src/programmers/level2/위장.java
+++ src/programmers/level2/위장.java
@@ -1,31 +1,26 @@
 package programmers.level2;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class 위장 {
     public int solution(String[][] clothes) {
-    	Map<String, List<String>> map = new HashMap<>();
+    	Map<String, Integer> map = new HashMap<>();
     	int N = clothes.length;
-    	List<String> group;
+    	String group;
     	for (int i = 0; i < N; i++) {
-			group = map.get(clothes[i][1]);
-    		if(group == null) {
-    			List<String> list = new ArrayList<>();
-    			list.add(clothes[i][0]);
-    			map.put(clothes[i][1], list);
+			group = clothes[i][1];
+    		if(map.containsKey(group)) {
+    			map.put(group, map.get(group)+1);
 			}
     		else {
-    			group.add(clothes[i][0]);
+    			map.put(group, 1);
     		}
 		}
     	
     	int answer = 1;
-    	for (List<String> items : map.values()) {
-    		int len = items.size();
-    		answer *= len+1;
+    	for (int n : map.values()) {
+    		answer *= n+1;
     	}
     	
         return answer-1;
