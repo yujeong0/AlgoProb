d558392c158f7401e3eaa5b2cb2ba67bc02ab318
DESKTOP-KVCQHCD\multicampus <kos9811@naver.com>
2021-01-18 00:52:17 +0900

    프로그래머스 문자열압축

--- src/programmers/level2/문자열압축.java
+++ src/programmers/level2/문자열압축.java
@@ -1,53 +1,45 @@
 package programmers.level2;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class 문자열압축 {
     public int solution(String s) {
-        int answer = 0;
-        
-        for (int len = 0; len < s.length(); len++) {
+        for (int len = 1; len <= s.length()/2; len++) {
 			cut(len, s);
 		}
-        return answer;
+        if(MIN > s.length()) MIN = s.length();
+        return MIN;
     }
     
+    int MIN = Integer.MAX_VALUE;
     public void cut(int len, String s) {
-    	Map<String, Integer> map = new HashMap<>();
-    	for (int i = 0; i < s.length(); i+=len) {
-    		String cur;
-    		if(i+len <= s.length()) {
-    			cur = s.substring(i, i+len);
-    		}
-    		else cur = s.substring(i);
-    		
-    		map.put(cur, 1);
-    		int j = i+len;
-    		int cnt = 0;
-    		String next;
-    		while(true) {
-    			if(j+len <= s.length()) {
-    				next = s.substring(j, j+len);
-        		}
-        		else next = s.substring(j);
-    			
-				if(cur.equals(next)) {
-					map.replace(cur, map.get(cur)+1);
-					cnt++;
-				}
-				else break;
-			}
-    		
-    		for (int k = 0; k < cnt; k++) {
-				i += len;
-			}
-		}
-    }
-    
-    void getLength(Map<String, Integer> map) {
     	StringBuilder sb = new StringBuilder();
+    	String cur = s.substring(0, len);
+    	String next;
+    	int count = 1;
+    	for (int i = len; i < s.length(); i+=len) {
+    		if(i+len <= s.length()) next = s.substring(i, i+len);
+    		else next = s.substring(i);
+    		
+			if(cur.equals(next)) {
+				count++;
+			}
+			else {
+				if(count > 1) sb.append(count).append(cur);
+				else sb.append(cur);
+				
+				cur = next;
+				count = 1;
+			}
+    	}
+		if(count > 1) sb.append(count).append(cur);
+		else sb.append(cur);
     	
+    	if(MIN > sb.length()) MIN = sb.length();
     }
     
+    public static void main(String[] args) {
+		new 문자열압축().service();
+	}
+    public void service() {
+    	System.out.println(solution("aabbaccc"));
+    }
 }
