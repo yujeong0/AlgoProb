6e9850674bbb2a7b7656993903303eda5644231b
강유정 <kos9811@naver.com>
2021-01-20 22:47:35 +0900

    프로그래머스 최솟값 만들기

---
+++ src/programmers/level2/최솟값만들기.java
@@ -0,0 +1,18 @@
+package programmers.level2;
+
+import java.util.Arrays;
+
+public class 최솟값만들기 {
+	public int solution(int[] A, int[] B) {
+		int answer = 0;
+
+		Arrays.sort(A);
+		Arrays.sort(B);
+		
+		for (int i = 0; i < A.length; i++) {
+			answer += A[i] * B[B.length-1-i];
+		}
+		
+		return answer;
+	}
+}
