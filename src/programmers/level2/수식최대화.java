adccb3bb2f74eb16f20e829054747ce108e7484e
강유정 <kos9811@naver.com>
2021-01-22 00:12:29 +0900

    프로그래머스 수식최대화

--- src/programmers/level2/수식최대화.java
+++ src/programmers/level2/수식최대화.java
@@ -1,16 +1,17 @@
 package programmers.level2;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class 수식최대화 {
-	
 	public static void main(String[] args) {
-		System.out.println(new 수식최대화().solution("2-990-5+2+3*2"));
+		System.out.println(new 수식최대화().solution("100-200*300-500+20"));
 	}
 	
 	int[] numbers = new int[3];
 	boolean[] selected = new boolean[3];
 	char[] operators = {'*', '+', '-'};
     public long solution(String expression) {
-        
         permu(0, expression);
         
         return answer;
@@ -19,58 +20,47 @@ public class 수식최대화 {
     long answer = 0;
     void permu(int cnt, String expression) {
     	if(cnt == 3) {
-        	for (int oper = 0; oper < 3; oper++) {
-        		for (int i = 0; i < expression.length(); i++) {
-        			if(i != 0 && expression.charAt(i) == operators[numbers[oper]]) {
-        				
-        				String prev, next;
-        				int cnt1 = 0;
-        				for (int j = i-1; j >= 0; j--) {
-        					if(expression.charAt(j) != '+' && expression.charAt(j) != '-' && expression.charAt(j) != '*') {
-        						cnt1++;
-        					} else if(j == 0 && expression.charAt(j) == '-') {
-        						cnt1++;
-        						break;
-        					}  else if(expression.charAt(j) == '-' && (expression.charAt(j-1) == '-' || expression.charAt(j-1) == '+'|| expression.charAt(j-1) == '*')) {
-        						cnt1++;
-        						break;
-        					} else break;
-						}
-        				if(cnt1 > 0) {
-	        				prev = expression.substring(i-cnt1, i);
-	        				
-	        				int cnt2 = 0;
-	        				for (int j = i+1; j < expression.length(); j++) {
-	        					if(expression.charAt(j) != '+' && expression.charAt(j) != '-' && expression.charAt(j) != '*') {
-	        						cnt2++;
-	        					} else if(cnt2 == 0) {
-	        						cnt2++;
-	        					} else break;
-	        				}
-	        				next = expression.substring(i+1, i+cnt2+1);
-	        				
-	        				long result = 0;
-	        				switch(operators[numbers[oper]]) {
-	        				case '*':
-	        					result = Integer.parseInt(prev) * Integer.parseInt(next);
-	        					break;
-	        				case '+':
-	        					result = Integer.parseInt(prev) + Integer.parseInt(next);
-	        					break;
-	        				case '-':
-	        					result = Integer.parseInt(prev) - Integer.parseInt(next);
-	        					break;
-	        				}
-	        				
-	        				expression = expression.substring(0, i-cnt1) + result + expression.substring(i+cnt2+1);
-	        				i = -1;
-	        			}
-        			}
-        		}
-			}
-    		
-        	if(answer < Math.abs(Long.parseLong(expression))) answer = Math.abs(Long.parseLong(expression));
+        	List<Long> numList = new ArrayList<>();
+        	List<Character> operList = new ArrayList<>();
         	
+        	for (int i = 0; i < expression.length(); i++) {
+				if(expression.charAt(i) == '+' || expression.charAt(i) == '-' || expression.charAt(i) == '*') operList.add(expression.charAt(i));
+				else {
+					int j = i;
+					while(true) {
+						if(j == expression.length()) break;
+						if(expression.charAt(j) == '+' || expression.charAt(j) == '-' || expression.charAt(j) == '*') break;
+						j++;
+					}
+					numList.add(Long.parseLong(expression.substring(i, j)));
+					i = j-1;
+				}
+			}
+        	
+        	for (int i = 0; i < 3; i++) {
+				for (int j = 0; j < operList.size(); j++) {
+					if(operList.get(j) == operators[numbers[i]]) {
+		        		Long tot = (long) 0;
+		        		switch(operators[numbers[i]]) {
+						case '*':
+							tot = numList.get(j) * numList.get(j+1); 
+							break;
+						case '+':
+							tot = numList.get(j) + numList.get(j+1); 
+							break;
+						case '-':
+							tot = numList.get(j) - numList.get(j+1); 
+							break;
+						}
+		        		numList.remove(j+1);
+		        		numList.set(j, tot);
+		        		operList.remove(j);
+		        		j = -1;
+					}
+				}
+			}
+        	
+        	if(answer < Math.abs(numList.get(0))) answer = Math.abs(numList.get(0));
     		return;
     	}
     	
