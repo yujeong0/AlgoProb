dea01a969c862f19d2f39e32dc3d4f1a512bbbdb
강유정 <kos9811@naver.com>
2021-01-28 00:36:34 +0900

    프로그래머스 올바른괄호

--- src/programmers/level2/올바른괄호.java
+++ src/programmers/level2/올바른괄호.java
@@ -1,6 +1,18 @@
 package programmers.level2;
 
 public class 올바른괄호 {
-
+    boolean solution(String s) {
+        int close = 0, idx = s.length()-1;
+        char c;
+        while(idx >= 0) {
+        	c = s.charAt(idx--);
+        	if(close == 0 && c == '(') return false;
+        	if(c == ')') close++;
+        	else if(close > 0 && c == '(') close--;
+        }
+        
+        if(close > 0) return false;
+        return true;
+    }
 
 }
