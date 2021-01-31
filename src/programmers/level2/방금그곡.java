4a21b3a6c56ddc8becc0e6c1221b41b2ce173a4d
강유정 <kos9811@naver.com>
2021-02-01 00:43:47 +0900

    프로그래머스 방금그곡

--- src/programmers/level2/방금그곡.java
+++ src/programmers/level2/방금그곡.java
@@ -4,10 +4,7 @@ public class 방금그곡 {	// 36분
 	public String solution(String m, String[] musicinfos) {
 		if(musicinfos.length == 0) return "(None)";
 		
-//		String[] original = {"C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"};
-//		char[] newArr 		{ 'A','B',  'C', 'D',  'E', 'F', 'G', 'H',  'I', 'J', 'K',  'L'};
-		
-		m = change(m);
+		m = m.replace("C#", "c").replace("D#", "d").replace("E#", "e").replace("F#", "f").replace("G#","g").replace("A#","a");
         String answer = "";
         int MAX = 0;
         for (int i = 0; i < musicinfos.length; i++) {
@@ -19,10 +16,12 @@ public class 방금그곡 {	// 36분
         		len = Integer.parseInt(t2[1]) - Integer.parseInt(t1[1]); 
         	}
         	else {
-        		len = 60 - Integer.parseInt(t1[1]) + Integer.parseInt(t2[1]); 
+        		len = 0;
+        		len += 60 * (Integer.parseInt(t2[0]) - Integer.parseInt(t1[0])); 
+    			len += Integer.parseInt(t2[1]) - Integer.parseInt(t1[1]);
         	}
         	
-        	info[3] = change(info[3]);
+        	info[3] = info[3].replace("C#", "c").replace("D#", "d").replace("E#", "e").replace("F#", "f").replace("G#","g").replace("A#","a");
 
         	StringBuilder sb = new StringBuilder();
 			int idx = 0;
@@ -43,48 +42,13 @@ public class 방금그곡 {	// 36분
         if(answer.length() == 0) {
         	return "(None)";
         }
+        
         return answer;
 	}
-	
-	public String change(String str) {
-		StringBuilder sb = new StringBuilder();
-    	for (int j = 0; j < str.length(); j++) {
-			switch(str.charAt(j)) {
-			case 'C':
-				if(j+1 < str.length() && str.charAt(j+1) == '#') sb.append('B');
-				else sb.append('A');
-				break;
-			case 'D':
-				if(j+1 < str.length() && str.charAt(j+1) == '#') sb.append('D');
-				else sb.append('C');
-				break;
-			case 'E':
-				sb.append('E');
-				break;
-			case 'F':
-				if(j+1 < str.length() && str.charAt(j+1) == '#') sb.append('G');
-				else sb.append('F');
-				break;
-			case 'G':
-				if(j+1 < str.length() && str.charAt(j+1) == '#') sb.append('I');
-				else sb.append('H');
-				break;
-			case 'A':
-				if(j+1 < str.length() && str.charAt(j+1) == '#') sb.append('K');
-				else sb.append('J');
-				break;
-			case 'B':
-				sb.append('L');
-				break;
-			}
-		}
-    	return sb.toString();
-	}
-    public static void main(String[] args) {
+
+	public static void main(String[] args) {
 //		System.out.println(new 방금그곡().solution("BC"
 //				, new String[] {"03:00,03:04,FOO,ABC#"}));
-//		System.out.println(new 방금그곡().solution("ABC"
-//				, new String[] {"13:00,13:05,WORLD,ABCDEF"}));
 		System.out.println(new 방금그곡().solution("ABC"
 				, new String[] {"12:00,12:14,HELLO,C#DEFGAB", "13:00,13:05,WORLD,ABCDEF"}));
 //		System.out.println(new 방금그곡().solution("ABCDEFG"
