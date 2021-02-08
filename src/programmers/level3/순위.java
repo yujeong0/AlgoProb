16468973920f6d92e45e3646b53e8908da8d372e
강유정 <kos9811@naver.com>
2021-02-09 00:25:57 +0900

    임시 commit

--- src/programmers/level3/순위.java
+++ src/programmers/level3/순위.java
@@ -1,57 +1,9 @@
 package programmers.level3;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class 순위 {
     public int solution(int n, int[][] results) {
-    	int[][] adjMatrix = new int [n+1][n+1];
-    	for (int i = 0; i < results.length; i++) {
-			adjMatrix[results[i][0]][results[i][1]] = 1;
-			adjMatrix[results[i][1]][results[i][0]] = 2;
-		}
-    	
-    	int answer = 0;
-    	List<Integer> complete = new ArrayList<>();
-    	for (int i = 1; i <= n; i++) {
-    		int zeroCnt = 0;
-    		for (int j = 1; j <= n; j++) {
-				if(i == j) continue;
-				if(adjMatrix[i][j] == 0) zeroCnt++;
-			}
-    		if(zeroCnt == 1) {
-    			answer++;
-    			complete.add(i);
-    		}
-    	}
-    	
-    	for (int i = 1; i <= n; i++) {
-    		boolean con = false;
-    		for(int num : complete) {
-    			if(num == i) {
-    				con = true;
-    				break;
-    			}
-    		}
-    		if(con) continue;
-    		
-    		for(int num : complete) {
-    			if(adjMatrix[i][num] != 0) {
-    				int lose =  0;
-    				for (int j = 1; j <= n; j++) {
-						if(adjMatrix[num][j] == 2) lose++;
-					}
-    				if(adjMatrix[i][num] == 2 && n-1-lose == 1) {
-    					answer++;
-    				}
-    				if(adjMatrix[i][num] == 1 && lose == n-2) {
-    					answer++;
-    				}
-    			}
-    		}
-    	}
-    	
-        return answer;
+        int answer = 0;
+		return answer;
     }
     
     public static void main(String[] args) {
