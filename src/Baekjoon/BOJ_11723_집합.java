895a847df47f694ae943596766ec480f476ded97
강유정 <kos9811@naver.com>
2021-06-07 14:59:47 +0900

    BOJ_11723_집합

--- src/Baekjoon/BOJ_11723_집합.java
+++ src/Baekjoon/BOJ_11723_집합.java
@@ -10,44 +10,45 @@ public class BOJ_11723_집합 {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int M = Integer.parseInt(br.readLine());
-//		Set<Integer> set = new HashSet<Integer>();
-		boolean[] arr = new boolean[21];
-		StringBuffer sb = new StringBuffer();
+		Set<Integer> set = new HashSet<Integer>();
+//		boolean[] arr = new boolean[21];
+		StringBuilder sb = new StringBuilder();
 		int x;
 		for (int t = 0; t < M; t++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			switch(st.nextToken()) {
 			case "add":
 				x = Integer.parseInt(st.nextToken());
-				arr[x] = true;
-//				set.add(x);
+//				arr[x] = true;
+				set.add(x);
 				break;
 			case "remove":
 				x = Integer.parseInt(st.nextToken());
-				arr[x] = false;
-//				set.remove(x);
+//				arr[x] = false;
+				set.remove(x);
 				break;
 			case "check":
 				x = Integer.parseInt(st.nextToken());
-//				System.out.println(arr[x]? 1 : 0);
-				sb.append(arr[x]? 1 : 0).append("\n");
-//				System.out.println(set.contains(x)? 1 : 0);
+//				sb.append(arr[x]? 1 : 0).append("\n");
+				sb.append(set.contains(x)? 1 : 0).append("\n");
 				break;
 			case "toggle":
 				x = Integer.parseInt(st.nextToken());
-				arr[x] = !arr[x];
-//				if(set.contains(x)) set.remove(x);
-//				else set.add(x);
+//				arr[x] = !arr[x];
+				if(set.contains(x)) set.remove(x);
+				else set.add(x);
 				break;
 			case "all":
 				for (int i = 1; i <= 20; i++) {
-					arr[i] = true;
+//					arr[i] = true;
+					set.add(i);
 				}
 				break;
 			case "empty":
-				for (int i = 1; i <= 20; i++) {
-					arr[i] = false;
-				}
+//				for (int i = 1; i <= 20; i++) {
+//					arr[i] = false;
+//				}
+				set.clear();
 				break;
 			}
 		} // t
