a63b1fd4316628b8d865d9a8e277c5c3bb73ff0e
강유정 <kos9811@naver.com>
2021-07-23 17:02:47 +0900

    BOJ_17825_주사위윷놀이

--- src/Baekjoon/BOJ_17825_주사위윷놀이.java
+++ src/Baekjoon/BOJ_17825_주사위윷놀이.java
@@ -7,7 +7,6 @@ import java.util.StringTokenizer;
 public class BOJ_17825_주사위윷놀이 {
 	static class Node {
 		int number, score, start, next;
-		
 		public Node(int number, int score, int next) {
 			this.number = number;
 			this.score = score;
@@ -21,40 +20,7 @@ public class BOJ_17825_주사위윷놀이 {
 			this.next = next;
 		}
 	}
-	static Node[] nodes = {
-			new Node(0,0,1),
-			new Node(1,2,2),
-			new Node(2,4,3),
-			new Node(3,6,4),
-			new Node(4,8,5),
-			new Node(5,10,6,10),
-			new Node(6,13,7),
-			new Node(7,16,8),
-			new Node(8,19,9),
-			new Node(9,25,29),
-			new Node(10,12,11),
-			new Node(11,14,12),
-			new Node(12,16,13),
-			new Node(13,18,14),
-			new Node(14,20,15,17),
-			new Node(15,22,16),
-			new Node(16,24,9),
-			new Node(17,22,18),
-			new Node(18,24,19),
-			new Node(19,26,20),
-			new Node(20,28,21),
-			new Node(21,30,22,25),
-			new Node(22,28,23),
-			new Node(23,27,24),
-			new Node(24,26,9),
-			new Node(25,32,26),
-			new Node(26,34,27),
-			new Node(27,36,28),
-			new Node(28,38,31),
-			new Node(29,30,30),
-			new Node(30,35,31),
-			new Node(31,40,32)
-	};
+
 	static int[] diceNums = new int[10];
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -135,4 +101,39 @@ public class BOJ_17825_주사위윷놀이 {
 		if(MAX < score) MAX = score;
 		
 	} // solve
+	
+	static Node[] nodes = {
+			new Node(0,0,1),
+			new Node(1,2,2),
+			new Node(2,4,3),
+			new Node(3,6,4),
+			new Node(4,8,5),
+			new Node(5,10,6,10),
+			new Node(6,13,7),
+			new Node(7,16,8),
+			new Node(8,19,9),
+			new Node(9,25,29),
+			new Node(10,12,11),
+			new Node(11,14,12),
+			new Node(12,16,13),
+			new Node(13,18,14),
+			new Node(14,20,15,17),
+			new Node(15,22,16),
+			new Node(16,24,9),
+			new Node(17,22,18),
+			new Node(18,24,19),
+			new Node(19,26,20),
+			new Node(20,28,21),
+			new Node(21,30,22,25),
+			new Node(22,28,23),
+			new Node(23,27,24),
+			new Node(24,26,9),
+			new Node(25,32,26),
+			new Node(26,34,27),
+			new Node(27,36,28),
+			new Node(28,38,31),
+			new Node(29,30,30),
+			new Node(30,35,31),
+			new Node(31,40,32)
+	};
 }
