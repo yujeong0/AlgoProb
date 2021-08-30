daad33e23f953eb3789b05b581eb82f1f305d22a
강유정 <kos9811@naver.com>
2021-08-30 23:30:19 +0900

    큐 확인

--- src/data_structure/_02_Queue_linkedlist.java
+++ src/data_structure/_02_Queue_linkedlist.java
@@ -1,6 +1,40 @@
 package data_structure;
 
-public class _02_Queue_arr {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+import data_structure._02_Queue_linkedlist.Queue;
+
+public class _02_Queue_linkedlist {
+
+	public static void main(String[] args) throws Exception {
+		new _02_Queue_linkedlist().solve();
+	}
+	
+	private void solve() throws Exception {
+		Queue<Integer> q = new Queue<Integer>();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		while(true) {
+			int a = Integer.parseInt(br.readLine());
+			
+			switch(a) {
+			case 1: {
+				int b = Integer.parseInt(br.readLine());
+				q.offer(b);
+				break;
+			}
+			case 2: {
+				System.out.println(q.poll());
+				break;
+			}
+			case 3: {
+				System.out.println(q.peek());
+				break;
+			}
+			}
+		}
+	
+	}
 
 	public class Node<E> {
 		E data;
